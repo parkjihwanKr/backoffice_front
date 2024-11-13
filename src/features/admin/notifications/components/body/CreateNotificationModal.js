93a5d46b3e36da496f1c4d89180f4f34b8e2e8ad
parkjihwan <mankcom7708@gmail.com>
2024-11-13 19:31:38 +0900

    fix : notification
    특정인 알림 발송 ui 수정
    
    #12

--- src/features/admin/notifications/components/body/CreateNotificationModal.js
+++ src/features/admin/notifications/components/body/CreateNotificationModal.js
@@ -1,39 +1,228 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { getCookie } from "../../../../../utils/CookieUtil";
 import CloseImageButton from "../../../../../components/ui/image/CloseImageButton";
 import './CreateNotificationModal.css';
-import {sendWebSocketBroadcastMessage, sendWebSocketMessage} from "../../../../../utils/WebSocketUtil";
+import {
+    departmentMapping,
+    positionMapping,
+    DEPARTMENTS,
+    imagePrefix
+} from "../../../../../utils/Constant";
+import { sendWebSocketBroadcastMessage, sendWebSocketMessage } from "../../../../../utils/WebSocketUtil";
+import { fetchMemberList } from "../../../members/services/MemberManagementService";
+import { adjustModalAlignment, addModalAlignmentListener } from "../../../../../utils/ModalUtils";
 
-const CreateNotificationModal = ({ title, isOpen, onClose, onSubmit }) => {
+const CreateNotificationModal = ({ title, isOpen, onClose, onSubmit, departments = DEPARTMENTS }) => {
     const [message, setMessage] = useState("");
+    const [selectedDepts, setSelectedDepts] = useState([]);
+    const [excludedUsers, setExcludedUsers] = useState([]);
+    const [memberList, setMemberList] = useState([]);
+    const [deptDropdownVisible, setDeptDropdownVisible] = useState(false);
+    const [userDropdownVisible, setUserDropdownVisible] = useState(false);
+
     const accessToken = getCookie('accessToken');
 
-    // 관리자 전용 단체 메세지 전송 핸들러
-    const handleSend = () => {
-        const payload = { message };  // 보낼 메시지 페이로드
+    const modalOverlayRef = useRef(null);
+    const modalContentRef = useRef(null);
 
-        sendWebSocketBroadcastMessage("/app/admins/notifications", payload, accessToken) // WebSocketUtil로 메시지 전송
-            .then(() => {
-                onSubmit(message); // 전송 후 콜백 호출
-                setMessage(""); // 메시지 필드 초기화
-            })
-            .catch(() => console.error("Failed to send message"));
+    useEffect(() => {
+        if (isOpen) {
+            fetchMemberList()
+                .then(data => setMemberList(data))
+                .catch(error => console.error("Error fetching member list:", error));
+
+            const modalOverlay = document.querySelector('.create-notification-modal-overlay');
+            const modalContent = document.querySelector('.create-notification-modal-content');
+            adjustModalAlignment(modalOverlay, modalContent);
+            return addModalAlignmentListener(modalOverlay, modalContent);
+        }
+    }, [isOpen]);
+
+    const handleSend = () => {
+        const payload = { message };
+
+        if (title === "전체 알림 발송") {
+            // 전체 알림 발송 로직
+            sendWebSocketBroadcastMessage("/app/admins/notifications", payload, accessToken)
+                .then(() => {
+                    onSubmit(message);
+                    setMessage(""); // 메시지 초기화
+                })
+                .catch((error) => console.error("Failed to send broadcast message:", error));
+        } else if (title === "특정 인원 알림 발송") {
+            // 특정 인원 알림 발송 로직
+            const filteredPayload = {
+                message,
+                excludedMemberIdList: excludedUsers.map(user => user.memberId), // 제외된 사용자 ID 리스트
+                excludedMemberDepartment: selectedDepts // 제외된 부서 리스트
+            };
+
+            sendWebSocketMessage("/app/admins/notifications/filtered", filteredPayload, accessToken)
+                .then(() => {
+                    onSubmit(message, excludedUsers, selectedDepts);
+                    setMessage(""); // 메시지 초기화
+                })
+                .catch((error) => console.error("Failed to send filtered notifications:", error));
+        }
     };
 
-    if (!isOpen) return null;
+    const toggleDeptSelection = (dept) => {
+        setSelectedDepts((prevSelected) =>
+            prevSelected.includes(dept)
+                ? prevSelected.filter((item) => item !== dept)
+                : [...prevSelected, dept]
+        );
+    };
+
+    const toggleUserSelection = (userName, userDepartment, userPosition) => {
+        setExcludedUsers((prevSelected) => {
+            const userExists = prevSelected.find(user => user.name === userName);
+            if (userExists) {
+                return prevSelected.filter(user => user.name !== userName);
+            } else {
+                return [...prevSelected, { name: userName, department: userDepartment, position: userPosition }];
+            }
+        });
+    };
+
+    const handleDeptDropdownAction = (action) => {
+        setDeptDropdownVisible(false);
+        if (action === "selectAll") setSelectedDepts(departments);
+        else if (action === "clearAll") setSelectedDepts([]);
+    };
+
+    const handleUserDropdownAction = (action) => {
+        setUserDropdownVisible(false);
+        if (action === "selectAll") {
+            const selectableUsers = memberList.filter(member => !selectedDepts.includes(member.department));
+            setExcludedUsers(selectableUsers.map(user => ({
+                name: user.memberName,
+                department: user.department,
+                position: user.position,
+            })));
+        } else if (action === "clearAll") {
+            setExcludedUsers([]);
+        }
+    };
+
+    const getMappedDepartment = (dept) => Object.keys(departmentMapping).find(key => departmentMapping[key] === dept) || dept;
+    const getMappedPosition = (position) => Object.keys(positionMapping).find(key => positionMapping[key] === position) || position;
 
     return (
-        <div className="create-notification-modal-overlay">
-            <div className="create-notification-modal-content">
+        <div ref={modalOverlayRef} className="create-notification-modal-overlay">
+            <div ref={modalContentRef} className="create-notification-modal-content">
                 <div className="create-notification-modal-header">
                     <h2>{title}</h2>
                     <CloseImageButton handleClose={onClose} />
                 </div>
+
+                {title === "특정 인원 알림 발송" && (
+                    <div className="selection-row">
+                        <div>
+                            <h5>
+                                제외할 부서 선택
+                                <img
+                                    src={`${imagePrefix}/shared/settings.png`}
+                                    alt="Settings"
+                                    className="dept-settings-icon"
+                                    onClick={() => setDeptDropdownVisible(!deptDropdownVisible)}
+                                />
+                            </h5>
+                            {deptDropdownVisible && (
+                                <div className="dept-settings-dropdown-menu">
+                                    <button onClick={() => handleDeptDropdownAction("selectAll")}>전체 선택</button>
+                                    <button onClick={() => handleDeptDropdownAction("clearAll")}>전체 선택 해제</button>
+                                </div>
+                            )}
+                            <div className="checkbox-list">
+                                {departments.map((dept) => (
+                                    <div
+                                        key={dept}
+                                        className={`checkbox-item ${selectedDepts.includes(dept) ? 'selected' : ''}`}
+                                        onClick={() => toggleDeptSelection(dept)}
+                                    >
+                                        {getMappedDepartment(dept)}
+                                        {selectedDepts.includes(dept) && <span className="checkmark">✔</span>}
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+
+                        <div>
+                            <h5>
+                                제외할 사용자 선택
+                                <img
+                                    src={`${imagePrefix}/shared/settings.png`}
+                                    alt="Settings"
+                                    className="user-settings-icon"
+                                    onClick={() => setUserDropdownVisible(!userDropdownVisible)}
+                                />
+                            </h5>
+                            {userDropdownVisible && (
+                                <div className="user-settings-dropdown-menu">
+                                    <button onClick={() => handleUserDropdownAction("selectAll")}>전체 선택</button>
+                                    <button onClick={() => handleUserDropdownAction("clearAll")}>전체 선택 해제</button>
+                                </div>
+                            )}
+                            <div className="checkbox-list">
+                                {memberList
+                                    .filter((member) => !selectedDepts.includes(member.department))
+                                    .map((user) => (
+                                        <div
+                                            key={user.memberId}
+                                            className={`checkbox-item ${
+                                                excludedUsers.some(selected => selected.name === user.memberName)
+                                                    ? 'selected'
+                                                    : ''
+                                            }`}
+                                            onClick={() =>
+                                                toggleUserSelection(user.memberName, user.department, user.position)
+                                            }
+                                        >
+                                            <div className="user-name">{user.memberName}</div>
+                                            <div className="user-details">
+                                                ({getMappedDepartment(user.department)}, {getMappedPosition(user.position)})
+                                            </div>
+                                            {excludedUsers.some(selected => selected.name === user.memberName) && (
+                                                <span className="checkmark">✔</span>
+                                            )}
+                                        </div>
+                                    ))}
+                            </div>
+                        </div>
+                    </div>
+                )}
+
+                {title === "특정 인원 알림 발송" && (
+                    <div className="selected-lists-container">
+                        <div className="selected-departments-list">
+                            <h3>제외할 부서:</h3>
+                            <ul>
+                                {selectedDepts.map((dept, index) => (
+                                    <li key={index}>{getMappedDepartment(dept)}</li>
+                                ))}
+                            </ul>
+                        </div>
+                        <div className="selected-users-list">
+                            <h3>제외할 사용자:</h3>
+                            <ul>
+                                {excludedUsers.map((user, index) => (
+                                    <li key={index}>
+                                        {user.name} ({getMappedDepartment(user.department)}, {getMappedPosition(user.position)})
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    </div>
+                )}
+
                 <textarea
+                    className="message-input"
                     placeholder="알림 내용을 입력하세요"
                     value={message}
                     onChange={(e) => setMessage(e.target.value)}
                 />
+
                 <div className="create-notification-modal-buttons">
                     <button
                         onClick={handleSend}
@@ -43,7 +232,6 @@ const CreateNotificationModal = ({ title, isOpen, onClose, onSubmit }) => {
                 </div>
             </div>
         </div>
-
     );
 };
 
--- src/features/admin/notifications/components/body/NotificationManagementBody.js
+++ src/features/admin/notifications/components/body/NotificationManagementBody.js
@@ -1,6 +1,8 @@
-// NotificationManagementBody.js
-import React, { useState } from 'react';
+// NotifcationManagement.js
+import React, { useState, useEffect } from 'react';
 import CreateNotificationModal from './CreateNotificationModal';
+import { DEPARTMENTS } from '../../../../../utils/Constant';
+import { fetchMemberList } from '../../../members/services/MemberManagementService';
 import './NotificationManagementBody.css';
 
 const NotificationManagementBody = () => {
@@ -8,6 +10,20 @@ const NotificationManagementBody = () => {
     const [modalTitle, setModalTitle] = useState("");
     const [message, setMessage] = useState("");
     const [onSubmit, setOnSubmit] = useState(() => () => {});
+    const [members, setMembers] = useState([]);
+
+    useEffect(() => {
+        // 서버에서 사용자 목록을 불러와 설정
+        const loadMembers = async () => {
+            try {
+                const memberList = await fetchMemberList();
+                setMembers(memberList);
+            } catch (error) {
+                console.error("Error loading member list:", error);
+            }
+        };
+        loadMembers();
+    }, []);
 
     const handleOpenAllModal = () => {
         setModalTitle("전체 알림 발송");
@@ -17,13 +33,16 @@ const NotificationManagementBody = () => {
 
     const handleOpenSpecificModal = () => {
         setModalTitle("특정 인원 알림 발송");
-        setOnSubmit(() => (msg) => alert(`특정 사용자에게 알림: ${msg}`));
+        setOnSubmit(() => (msg, excludedUser, selectedDept) => {
+            alert(`부서: ${selectedDept}, 제외 사용자: ${excludedUser}를 제외하고 알림: ${msg}`);
+            // 서버 전송 로직 추가 가능
+        });
         setIsModalOpen(true);
     };
 
     const handleCloseModal = () => {
         setIsModalOpen(false);
-        setMessage(""); // 메시지 초기화
+        setMessage("");
     };
 
     return (
@@ -49,6 +68,8 @@ const NotificationManagementBody = () => {
                     isOpen={isModalOpen}
                     onClose={handleCloseModal}
                     onSubmit={onSubmit}
+                    departments={DEPARTMENTS} // 상수에서 불러온 부서 리스트 전달
+                    members={modalTitle === "특정 인원 알림 발송" ? members : []} // 특정 인원일 때만 members 사용
                 />
             )}
         </div>
